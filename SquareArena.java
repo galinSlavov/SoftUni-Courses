17b8a1e4811cfc4c06c289f8b2dd9b537f73d6b9
Galin Slavov <89240010+galinSlavov@users.noreply.github.com>
2021-08-20 13:21:45 +0300

    myFirst

---
+++ SquareArena.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class SquareArena {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = Integer.parseInt(scan.nextLine());
+        int area = a * a;
+        System.out.println(area);
+
+
+    }
+}
