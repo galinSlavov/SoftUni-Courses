c1c6a01bd702bd0995ef386d8c37ee914b575199
Galin Slavov <89240010+galinSlavov@users.noreply.github.com>
2021-09-09 11:50:47 +0300

    add
    Add basic project concentratedData, nums1To10, inchToCm, squareArea, greetings

---
+++ P04InchestoCentimeters.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class P04InchestoCentimeters {
+    public static void main(String[] args) {
+        Scanner scanner =  new Scanner(System.in);
+
+        double inch  = Double.parseDouble(scanner.nextLine());
+
+        double cm = inch * 2.54;
+
+        System.out.println(cm);
+
+    }
+}
