6936cffa276544d1604f61861a5a191215825820
Galin Slavov <89240010+galinSlavov@users.noreply.github.com>
2021-10-07 10:25:36 +0300

    add
    Added exercise 6 in Java Fundamentals course.

---
+++ javaFundamentals/exercise07/E01CountCharsInString.java
@@ -0,0 +1,12 @@
+package exercise07;
+
+import java.util.Scanner;
+
+public class E01CountCharsInString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
+}
